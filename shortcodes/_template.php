3857dc30e493d5d9ab89eaa9cbc97465f5909095
David Steinbauer <dsteinbauer@gmail.com>
2017-03-06 21:34:18 +0100

    Sets a template for all new Shortcodes

---
+++ shortcodes/_template.php
@@ -0,0 +1,9 @@
+<?php
+/* ======================= XXX SHORTCODE ======================= */
+//add_shortcode( 'av_xxx', 'avia_xxx' );
+if(!function_exists('xxx'))
+{
+
+}
+
+/* ======================= END XXX SHORTCODE ======================= */
