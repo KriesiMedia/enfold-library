dd94f7e879e7573fdac242cd9cf35ed73a64610c
InoPlugs <gs@schoenmann.at>
2017-04-11 14:37:52 +0200

    Add filter avf_postslider_show_catergories

---
+++ filters/avf_enfold_postslider_show_catergories.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Allow to change default output of categories
+ * ============================================
+ * 
+ * by default supressed for setting Default(Business) blog style
+ * 
+ * 
+ * Copy the following snippet in functions.php and modify output to your need
+ * 
+ * @param string $show						
+ * @param string $blogstyle						'' | 'elegant-blog' | 'elegant-blog modern-blog'
+ * @param avia_sc_postslider $postslider		
+ * @return string								'show_elegant' | 'show_business' | 'use_theme_default' | 'no_show_cats' 
+ * 
+ * @version 1.0.0
+ * @requires Enfold 4.0.6
+ */
+function custom_autoresponse_email( $show, $blogstyle, avia_sc_postslider $postslider )
+{
+	/*
+	 * Modify $show to your need - see @return - defaults to 'use_theme_default'
+	 */
+	
+	return $show;
+}
+
+add_filter( 'avf_postslider_show_catergories', 'custom_autoresponse_email', 10, 4 );
