ab9889da944124a76a6d8e67035f087d8d609946
InoPlugs <gs@schoenmann.at>
2017-09-22 09:36:18 +0200

    Small changes

--- actions and filters/ALB Elements/Contact Form/avf_form_send.php
+++ actions and filters/ALB Elements/Contact Form/avf_form_send.php
@@ -14,11 +14,11 @@ add_filter('avf_form_send', 'my_avf_form_send', 10, 4 );
  * 
  * @param boolean $send
  * @param array $post			all $_POST with "avia_" removed
- * @param array $form_params
- * @param avia_form $object
+ * @param array $form_params	all form parameters defined in shortcode
+ * @param avia_form $avia_form
  * @return boolean				true to continue, false to stop sending email
  */
-function my_avf_form_send( $send, $post, $form_params, $object )
+function my_avf_form_send( $send, $post, $form_params, $avia_form )
 {
 	//	Do something here
 	
