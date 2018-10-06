506f13c17c39579a0612f8d71178433872255b47
InoPlugs <support@inoplugs.com>
2018-10-06 14:13:56 +0200

    Fix for old Envato theme updater API

--- temp_fixes/Enfold_4_4_1/class-pixelentity-theme-update.php
+++ temp_fixes/Enfold_4_4_1/class-pixelentity-theme-update.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * This class is based on the Envato API < 3.0 which will be outdated in near future.
+ * Will be replaced by Avia_Theme_Updater class that supports Envato API 3.0
+ * 
+ */
 if ( ! defined( 'ABSPATH' ) ) {  exit;  }    // Exit if accessed directly
 
 
@@ -8,6 +12,16 @@ if (!class_exists("PixelentityThemeUpdate")) {
 			
 		protected $username;
 		protected $apiKey;
+		
+		/**
+		 * function wp_update_themes calls filter pre_set_site_transient_update_themes twice during theme version check.
+		 * As we have a rate limiting by Envato we cache the result here
+		 * 
+		 * @since 4.4.3
+		 * @var array 
+		 */
+		protected $envato_results_cache;
+		
 
 		public function __construct($username,$apiKey,$authors) {
 			// to debug
@@ -16,8 +30,14 @@ if (!class_exists("PixelentityThemeUpdate")) {
 			$this->username = $username;
 			$this->apiKey = $apiKey;
 			$this->authors = $authors;
+			
+			$this->envato_results_cache = null;
 
 			add_filter("pre_set_site_transient_update_themes", array(&$this,"check"));
+		}
+		
+		public function __destruct() {
+			unset( $this->envato_results_cache );
 		}
 
 		public function check($updates) {
@@ -31,6 +51,19 @@ if (!class_exists("PixelentityThemeUpdate")) {
 			}
 
 			if (!$this->username || !$this->apiKey || !isset($updates->checked)) return $updates;
+			
+			/**
+			 * If we have already cached the result from Envato we can take this and avoid multiple requests for the same data
+			 */
+			if( is_array( $this->envato_results_cache ) )
+			{
+				foreach( $this->envato_results_cache as $theme => $update ) 
+				{
+					$updates->response[ $theme ] = $update;
+				}
+				
+				return $updates;
+			}
 
 			if (!class_exists("Envato_Protected_API")) {
 				require_once("class-envato-protected-api.php");
@@ -79,6 +112,8 @@ if (!class_exists("PixelentityThemeUpdate")) {
 				$filtered[$theme->Name] = $theme;
 			}
 
+			$this->envato_results_cache = array();
+			
 			foreach ($purchased as $theme) {
 			
 				if ( isset($theme->theme_name) && isset($filtered[$theme->theme_name])) {
@@ -87,14 +122,17 @@ if (!class_exists("PixelentityThemeUpdate")) {
 					if (version_compare($current->Version, $theme->version, '<')) {
 						// bingo, inject the update
 						if ($url = $api->wp_download($theme->item_id)) {
-							$update = array(
+							if( is_string( $url ) )
+							{
+								$update = array(
 											"url" => "http://themeforest.net/item/theme/{$theme->item_id}",
 											"new_version" => $theme->version,
 											"package" => $url
 											);
 
-							$updates->response[$current->Stylesheet] = $update;
-
+								$updates->response[$current->Stylesheet] = $update;
+								$this->envato_results_cache[ $current->Stylesheet ] = $update;
+							}
 						}
 					}
 				}
