73d8bb99c8f65af27f3ccb3791ac03e61f5889b1
InoPlugs <support@inoplugs.com>
2019-04-30 09:20:10 +0200

    Update avf_customize_heading_settings.php

--- actions and filters/Layout/avf_customize_heading_settings.php
+++ actions and filters/Layout/avf_customize_heading_settings.php
@@ -1,24 +1,33 @@
 <?php
 
 /**
- * This is an example only
- * =======================
+ * Allows to change the default <h1> - <h3> tags used by Enfold
  * 
  * @param array $args
- * @param string $context
- * @param array $extra_args
+ * @param string $context			where the filter is called from
+ * @param array $extra_args			contains extra info depending on $context
  * @return array
  */
 function my_avf_customize_heading_settings( array $args, $context, array $extra_args = array() )
 {
-	
+	/**
+	 * This is an example of the usage for "avia_sc_timeline"
+	 */
 	if( $context == 'avia_sc_timeline' )
 	{
-		$args = array(
-								'heading'		=> 'h1',
-								'extra_class'	=> ''
-							);
+		$args['heading'] = 'h1';						//	change heading from h3 to h1
+		$args['extra_class'] = 'my-timeline-class';		//	add an extra class for styling
 	}
+	
+	/**
+	 * This is an example of the usage for "avia_content_slider" and slider title
+	 */
+	if( 'avia_content_slider' == $context && is_array( $extra_args ) && in_array( 'slider_title', $extra_args ) )
+	{
+		$args['heading'] = 'h2';						//	change heading from h3 to h2
+		$args['extra_class'] = 'my-extra-class';		//	add an extra class for styling
+	}
+	
 	return $args;
 }
 
