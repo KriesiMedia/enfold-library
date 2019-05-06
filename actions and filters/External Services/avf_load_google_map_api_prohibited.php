9e243e206b3afa1001dbc59ea57cf1976553ca49
InoPlugs <support@inoplugs.com>
2019-05-06 17:18:48 +0200

    Add avf_load_google_map_api_prohibited.php

---
+++ actions and filters/External Services/avf_load_google_map_api_prohibited.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Unconditionally exclude Google Maps from site
+ * 
+ * @param boolean $no_load
+ * @return boolean
+ */
+function custom_no_load_google_map_api( $no_load )
+{
+	return true;
+}
+
+add_filter( 'avf_load_google_map_api_prohibited', 'custom_no_load_google_map_api', 10, 1 );
+
