738ae50a72fe2fd5eb1bde2ed5d284a6d21dd26f
InoPlugs <support@inoplugs.com>
2019-06-18 12:21:32 +0200

    Add avf_colorpicker_colors.php

---
+++ actions and filters/Option Settings/ALB Element Option Settings/avf_colorpicker_colors.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Add your own predefined colors to the popup colorpicker in ALB elements
+ * 
+ * @since 4.5.7.2
+ * @param array $colors
+ * @return array
+ */
+function custom_colorpicker_colors( array $colors )
+{
+	/**
+	 * These are the default colors - change or extend them as needed
+	 * Keep in mind that the more colors you use the smaller the boxes will be
+	 */
+	$colors = array( '#000000', '#ffffff', '#B02B2C', '#edae44', '#eeee22', '#83a846', '#7bb0e7', '#745f7e', '#5f8789', '#d65799', '#4ecac2' );
+	
+	return $colors;
+}
+
+	
+add_filter( 'avf_colorpicker_colors', 'custom_colorpicker_colors', 10, 1 );
+
+
