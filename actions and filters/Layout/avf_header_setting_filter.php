f2dbbbd8792d3c11b8c4cee20c56a8ee8b796847
InoPlugs <support@inoplugs.com>
2019-12-09 11:41:08 +0100

    Add avf_header_setting_filter.php

---
+++ actions and filters/Layout/avf_header_setting_filter.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * WARNING: This is a filter for developers only. Be careful making modifications as this might break the output,
+ * 
+ * @param array $header
+ * @param string $context		'setting_header' | 'setting_sidebar'
+ * @return array
+ */
+function my_header_setting_filter( $header, $context )
+{
+	/**
+	 * Example:
+	 * 
+	 * To modify the id for an alternate menu to 51:
+	 */
+	$header['alternate_menu'] = 51;
+	
+	return $header;
+}
+
+add_filter( 'avf_header_setting_filter', 'my_header_setting_filter', 10, 2 );
