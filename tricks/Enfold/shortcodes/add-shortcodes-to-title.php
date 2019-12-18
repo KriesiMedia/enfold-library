9cde8568a5d8d952b4eb989083d683643f25e964
InoPlugs <support@inoplugs.com>
2019-12-18 13:48:29 +0100

    Add add-shortcodes-to-title.php

---
+++ tricks/Enfold/shortcodes/add-shortcodes-to-title.php
@@ -0,0 +1,18 @@
+<?php
+
+/* 
+ * The following snippet shows how to add a shortcode to title and have a correct markup
+ */
+
+add_shortcode( 'sc_avia_test_head', 'avia_test_head' );
+add_filter( 'the_title', 'avia_add_shortcode' );
+
+function avia_test_head( $atts, $content = '', $shortcodename = '' )
+{
+	return 'text of sc_av_test_head';
+}
+
+function avia_add_shortcode( $title )
+{
+	return $title . ' - ' . do_shortcode( '[sc_avia_test_head]' );
+}
