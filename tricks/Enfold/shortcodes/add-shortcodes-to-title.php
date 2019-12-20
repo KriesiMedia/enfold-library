4d90947226273b3354b3da3cf803a1768824ae53
InoPlugs Webdesign <support@inoplugs.com>
2019-12-20 13:12:29 +0100

    Update add-shortcodes-to-title.php

--- tricks/Enfold/shortcodes/add-shortcodes-to-title.php
+++ tricks/Enfold/shortcodes/add-shortcodes-to-title.php
@@ -5,14 +5,14 @@
  */
 
 add_shortcode( 'sc_avia_test_head', 'avia_test_head' );
-add_filter( 'the_title', 'avia_add_shortcode', 999999, 1 );
+add_filter( 'the_title', 'avia_add_shortcode', 999999, 1, 2 );
 
 function avia_test_head( $atts, $content = '', $shortcodename = '' )
 {
 	return 'text of sc_av_test_head';
 }
 
-function avia_add_shortcode( $title )
+function avia_add_shortcode( $title, $post_id )
 {
 	return $title . ' - ' . do_shortcode( '[sc_avia_test_head]' );
 }
