49646fff203e11e3e4b3bb398722772fbf4cb76a
InoPlugs <support@inoplugs.com>
2020-01-22 16:44:55 +0100

    Update avf_privacy_cookie_infos.php

--- actions and filters/Privacy and Cookies/avf_privacy_cookie_infos.php
+++ actions and filters/Privacy and Cookies/avf_privacy_cookie_infos.php
@@ -1,7 +1,22 @@
 <?php
 
 /* 
- * Allows to add info for custom cookies or cookies not handled by Enfold
+ * Allows to add info for 3rd party cookies not handled by Enfold.
+ * 
+ * To display cookie info in default privacy modal popup window add the following to functions.php:
+ * 
+ * add_theme_support( 'avia_privacy_show_cookie_info' );
+ * 
+ * or use shortcode wherever you want to display it:
+ * 
+ * [av_privacy_cookie_info]
+ * 
+ * To allow wildcards at beginning or end use *:
+ *		wp-settings-time-*		will get all cookies starting with wp-settings-time-
+ *		*settings*				will get all cookies containing settings
+ *		*-settings-time			will get all cookies ending with -settings-time
+ * 
+ * The following lines show you examples how to use wildcards and this filter
  * 
  * @since 4.5.7.2
  * @param array $infos
@@ -11,10 +26,16 @@
 
 function my_custom_privacy_cookie_infos( $infos, $av_privacy_object )
 {
+	$infos['your_cookie_name' ] = __( 'An exactly match', 'avia_framework' );
+	
 	$user = get_current_user_id();
 	
-	$infos['wp-settings-time-' . $user ] = __( 'A WP settings time cookie', 'avia_framework' );
-	$infos['wp-settings-' . $user ] = __( 'A WP settings cookie', 'avia_framework' );
+//	$infos['wp-settings-time-' . $user ] = __( 'A WP settings time cookie', 'avia_framework' );
+//	$infos['wp-settings-' . $user ] = __( 'A WP settings cookie', 'avia_framework' );
+	
+	$infos['your_cookie_name*'] =  __( 'A cookie ends with', 'avia_framework' );
+	$infos['*your_cookie_name'] =  __( 'A cookie starts with', 'avia_framework' );
+	$infos['*your_cookie_name*'] =  __( 'A cookie contains', 'avia_framework' );
 			
 	return $infos;
 }
