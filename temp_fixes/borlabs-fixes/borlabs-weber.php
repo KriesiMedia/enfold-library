cb8ed8e9a587528b388ad1bf863a54a47a5976c5
InoPlugs <support@inoplugs.com>
2021-10-02 11:50:33 +0200

    add borlabs-fixes/borlabs-weber.php

---
+++ temp_fixes/borlabs-fixes/borlabs-weber.php
@@ -0,0 +1,51 @@
+<?php
+
+//Auszug aus Enfold.php von Borlabs Cookie
+function modifyVideoOutput( $output, $atts, $content, $shortcodename, $meta, $video_html_raw )
+{
+    if( ! empty( $atts['src'] ) ) 
+	{
+		$style = '';
+		
+		if( $atts['attachment'] )
+		{
+			$fallback = wp_get_attachment_image_src( $atts['attachment'], $atts['attachment_size']  );
+			if( is_array( $fallback ) )
+			{
+				$fallback_img = $fallback[0];
+				$style .= " background-image:url(\"{$fallback_img}\");";
+			}
+		}
+
+		if( ! empty( $atts['format'] ) && $atts['format'] == 'custom' ) 
+		{
+			$height = intval( $atts['height'] );
+			$width = intval( $atts['width'] );
+			$ratio = ( 100 / $width ) * $height;
+			$style .= " padding-bottom:{$ratio}%;";
+		}
+
+		if( ! empty( $style ) )
+		{
+			$style = "style='{$style}'";
+		}
+
+		if( ! empty( $atts['conditional_play'] ) && $atts['conditional_play'] === 'lightbox') 
+		{
+			// Nothing for now - can not be supported
+		} 
+		// Hier verändern die das originale Elternelement !
+		else 
+		{
+			$output = '<div class="avia-video avia-video-' . $atts['format'] . '" ' . $style . '>' . $video_html_raw . '</div>';
+		}
+    }
+	
+    return $output;
+}
+
+//	we hook after Borlabs and extend their modifications again
+add_filter( 'avf_sc_video_output', 'modifyVideoOutput', 110, 6 );
+
+
+
