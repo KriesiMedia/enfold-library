749ee54faca02770b65bd37b9b903a884df88fbb
InoPlugs Webdesign <support@inoplugs.com>
2021-11-05 17:56:35 +0100

    Update avf_media_gallery_sortable_filesize.php

--- actions and filters/Images and Lightbox/avf_media_gallery_sortable_filesize.php
+++ actions and filters/Images and Lightbox/avf_media_gallery_sortable_filesize.php
@@ -4,7 +4,14 @@
  * Filesize is stored in a postmeta.
  * For new uploaded files this is set automatically and when viewing files in listview it is updated for already existing files.
  *
- * We do not make that by default as this might lead to a timeout when a site has thousands of files in the media gallery.
+ * We do not create the filesize postmeta by default as this might lead to a timeout problem for sites which have thousands of files in the media gallery.
+ *
+ * To create the postmeta manually:
+ *   - Goto Media Library -> List View
+ *   - Click tab "Screen Options" on right top
+ *   - Option "Number of items per page" enter a very high number
+ *   - Click Button "Apply" - this reloads the page and all displayed media library items show filesize and postmeta is saved
+ *   - Now load all other pages - this will save filesize postmeta for these elements
  *
  * @since 4.8.7.2
  */
