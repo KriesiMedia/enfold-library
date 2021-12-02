a5cfd655695d48f593d0eff85abc6f51f491599a
vatoyiit <vatoyiit@users.noreply.github.com>
2021-12-02 16:57:18 +0200

    Update avf_magazine_header_content.php

--- actions and filters/ALB Elements/Magazine/avf_magazine_header_content.php
+++ actions and filters/ALB Elements/Magazine/avf_magazine_header_content.php
@@ -13,10 +13,10 @@ add_filter('avf_magazine_header_content', 'new_avf_magazine_header_content', 10,
 function new_avf_magazine_header_content($header_content, $entry){
 	
 	$author_field = $header_content['author'];
+
+	unset( $header_content['author'] );
 	
-  unset( $header_content['author'] );
-	
-  $header_content['author'] = $author_field;
+	$header_content['author'] = $author_field;
 	
 	return $header_content;
 	
