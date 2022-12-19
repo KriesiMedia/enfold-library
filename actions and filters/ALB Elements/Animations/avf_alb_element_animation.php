62b0af72243f7d9be9c9291b6070d5ce909e0be1
InoPlugs Webdesign <support@inoplugs.com>
2022-12-19 16:36:06 +0100

    Update avf_alb_element_animation.php

--- actions and filters/ALB Elements/Animations/avf_alb_element_animation.php
+++ actions and filters/ALB Elements/Animations/avf_alb_element_animation.php
@@ -7,6 +7,7 @@
  * similar to enfold\js\shortcodes.js function activate_waypoints().
  *
  * There you also find all theme predefined classes (extended in 5.3).
+ * Since 5.3 changed custom class to support any value between 0 and 99
  *
  *
  * @since 5.3
@@ -25,8 +26,9 @@ function custom_alb_element_animation( $class_animation, array $atts, aviaShortc
 		return $class_animation;
 	}
 
-	//	return a new waypoint animation class
-	return '.av-animated-when-visible-95';
+	//	return a new custom waypoint animation class (replace 95 with a value of your choice 0 - 99)
+	//	will be interpreted as from "top" of screen
+	return 'av-custom-animated-top-95';
 }
 
 add_filter( 'avf_alb_element_animation', 'custom_alb_element_animation', 10, 4 );
