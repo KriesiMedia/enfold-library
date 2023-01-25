89091af76c5a6c1e5022c0dad91de0ab79fcc8fd
InoPlugs Webdesign <support@inoplugs.com>
2023-01-25 15:46:25 +0100

    Update avia_social_share_link_arguments.php

--- actions and filters/ALB Elements/Social Buttons/avia_social_share_link_arguments.php
+++ actions and filters/ALB Elements/Social Buttons/avia_social_share_link_arguments.php
@@ -7,13 +7,20 @@
  * TikTok share icon has to be added before it can be added to Share Box: https://kriesi.at/documentation/enfold/social-share-buttons/#how-to-add-custom-social-icons-to-enfold-options
  *
  */
-add_filter('avia_social_share_link_arguments', 'avia_add_social_share_link_arguments', 10, 1);
+function custom_add_social_share_link_arguments( array $args = [] )
+{
+	$tiktok = array(
+				'tiktok' => array(
+						'encode'		=> true,
+						'encode_urls'	=> false,
+						'pattern'		=> 'https://www.tiktok.com/',
+						'label'			=> __( 'Share on TikTok', 'avia_framework' )
+					)
+			);
 
-function avia_add_social_share_link_arguments($args){
-  
-    $tiktok = array('tiktok' => array("encode"=>true, "encode_urls"=>false, "pattern" => "https://www.tiktok.com/", 'label' => __("Share on TikTok",'avia_framework')));
-  
-    $args = array_merge($tiktok, $args);
-  
-    return $args;
+	$args = array_merge( $tiktok, $args );
+
+	return $args;
 }
+
+add_filter( 'avia_social_share_link_arguments', 'custom_add_social_share_link_arguments', 10, 1 );
