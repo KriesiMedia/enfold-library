8abe7f75096d7c972f72f0740f8a8c5e75e91528
InoPlugs <support@inoplugs.com>
2023-04-03 12:30:21 +0200

    add avf_widget_loader_widget_classes.php

---
+++ actions and filters/Widgets/avf_widget_loader_widget_classes.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Snippet to remove or modify existing snippets or add a new custom widget
+ *
+ * @since 5.5
+ */
+
+function custom_avf_widget_loader_widget_classes( array $default_widgets )
+{
+	$namespace = '\\aviaFramework\widgets\\';
+
+	//	create a folder widgets in enfold-child
+	$path = trailingslashit( get_stylesheet_directory() ) . 'widgets/';
+
+	//	remove portfoliobox widget (= a default widget)
+	unset( $default_widgets['portfoliobox'] );
+
+	// add your child theme modified portfoliobox widget
+	$default_widgets['portfoliobox'] = array(
+												'class'	=> $namespace . 'avia_portfoliobox',
+												'file'	=> $path . 'class-avia-portfoliobox.php'
+											);
+
+	//	add a new custom widget, wrap in namespace aviaFramework\widgets
+	$default_widgets['your-widget'] = array(
+												'class'	=> $namespace . 'your_widget',
+												'file'	=> $path . 'class-your-widget.php'
+											);
+
+	return $default_widgets;
+}
+
+add_filter( 'avf_widget_loader_widget_classes', 'custom_avf_widget_loader_widget_classes', 10, 1 );
+
